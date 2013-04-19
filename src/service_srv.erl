08e34b953113a5b93e88b2c56d4d6777e94a957b
Dmitry Karpov <d.karpov@i-free.com>
2013-04-19 13:46:59 +0000

    update starter

--- src/service_srv.erl
+++ src/service_srv.erl
@@ -6,6 +6,7 @@
 -define(SERVER, ?MODULE).
 -define(DEFAULT_APP, nconfig).
 -record(app_state,{
+          module :: atom(),
           name :: atom() | list(),
           state :: 'on' | 'off',
           mode = auto :: 'auto' | 'manual'
@@ -15,7 +16,7 @@
 %% API Function Exports
 %% ------------------------------------------------------------------
 
--export([start_link/1, start_service/2, stop_service/1, restart_service/2]).
+-export([start_link/0, start_service/2, stop_service/1, restart_service/2, status_services/0]).
 
 %% ------------------------------------------------------------------
 %% gen_server Function Exports
@@ -28,9 +29,9 @@
 %% API Function Definitions
 %% ------------------------------------------------------------------
 
--spec start_link( HandleServices :: list()) -> {ok, Pid :: pid()}.
-start_link(HandleServices) ->
-  gen_server:start_link({global, ?SERVER}, ?MODULE, [HandleServices], [])
+-spec start_link() -> {ok, Pid :: pid()}.
+start_link() ->
+  gen_server:start_link({global, ?SERVER}, ?MODULE, [], [])
 .
 
 -spec behaviour_info(callbacks) -> list().
@@ -38,12 +39,10 @@ behaviour_info(callbacks) ->
   [{start,   1},
    {stop,    1},
    {restart, 1},
-   {status,  0}
+   {status,  0},
+   {register,1}
   ].
 
-%% ------------------------------------------------------------------
-%% gen_server Function Definitions
-%% ------------------------------------------------------------------
 
 -spec start_service(ServiceName :: atom(), StartMode :: 'auto' | 'manual') -> ok | error.
 start_service(?DEFAULT_APP, _)->
@@ -63,33 +62,87 @@ restart_service(ServiceName, RestartMode)->
   gen_server:call({global, ?MODULE}, {restart_service, ServiceName, RestartMode})
 .
 
-init([HandleServices])->
-  %%TODO: autostart HandleServices when mode = 'auto'
+-spec status_services()->list().
+status_services()->
+  gen_server:call({global, ?MODULE}, status_all)
+.
+name(Handle, Params)->
+	%%is application?
+	case application:load(Handle) of
+	ok -> 
+		config_srv:apply(Handle), 
+		#app_state{module = application, name = Handle, mode = auto, state = off };
+	{error,{"no such file or directory",_}} ->
+  	  ModuleName = list_to_atom("handle_" ++ atom_to_list(Handle)),
+	  case code:which(ModuleName) of
+	    non_existing -> 
+               #app_state{module= ModuleName, name = '', state = error, mode = manual};
+	    _ ->
+	       case ModuleName:register(Params) of
+                 error -> [];
+                 {'EXIT', _}-> [];
+                 {ok, Name} -> #app_state{ module = ModuleName, name = Name, state = off, mode = auto}
+               end
+	  end;
+	{error,{already_loaded,_}}->
+		#app_state{module = application, name = Handle, mode = duplicate, state = off };
+	_->[]
+	end
+.
+
+app_start(X, all, Mode) when (X#app_state.state == off) and (X#app_state.mode == Mode) ->
+	     M = X#app_state.module,
+	     case M:start(X#app_state.name) of
+	       ok -> X#app_state{state = on};
+	       {error,{already_started,_}} -> X#app_state{state = on};
+	       _ ->  X#app_state{state = error}
+	     end
+;
+app_start(X, Name, Mode) when X#app_state.state == off, X#app_state.name == Name ->
+	     M = X#app_state.module,
+	     case M:start(X#app_state.name) of
+	       ok -> X#app_state{mode = Mode, state = on};
+	       {error,{already_started,_}} -> X#app_state{state = on, mode = Mode};
+	       _ ->  X#app_state{state = error}
+	     end
+;
+app_start(X, _, _) -> X.
+
+%% ------------------------------------------------------------------
+%% gen_server Function Definitions
+%% ------------------------------------------------------------------
+init([])->
+  HandleServices = [ name(Name, Config) || {Name, Config} <- config_srv:get_config()],
   {ok, HandleServices}
 .
 
 handle_call(all, _From, HandleServices) ->
   {reply, HandleServices, HandleServices};
+handle_call(status_all, _From, HandleServices) ->
+  {reply, HandleServices, HandleServices};
 handle_call({start_service, ServiceName, StartMode}, _From, HandleServices) ->
-  case [ X || X <- HandleServices, is_record(X, app_state), X#app_state.name =:= ServiceName] of
+  case [ X || X <- HandleServices, is_record(X, app_state), (X#app_state.name == ServiceName) or (ServiceName == all)] of
     [] -> application:load(ServiceName),
           config_srv:apply(ServiceName),
-	  application:start(ServiceName)
+	  application:start(ServiceName),
+          {reply, ok, HandleServices}
     ;
-    [App | _] -> 
-          case App:start([]) of
-            ok-> {reply, ok };
-            _-> 
-              NewHandleServices =
-              lists:map(
-                fun(X) when is_record(X, app_state), X#app_state.name == ServiceName ->
-                     X#app_state{mode = StartMode, state = on};
-                   (X) -> X
-                end,
+    _ -> 
+          NewHandleServices =
+          lists:map(
+%%                fun(X) when is_record(X, app_state), X#app_state.state == off, (X#app_state.name == ServiceName) or (ServiceName == all) ->
+%%                     M = X#app_state.module,
+%%                     case M:start(X#app_state.name) of
+%%                       ok -> X#app_state{mode = StartMode, state = on};
+%%                       {error,{already_started,_}} -> X#app_state{state = on};
+%%                       _ ->  X#app_state{state = error}
+%%                     end;
+%%                   (X) -> X
+%%                end,
+		fun(X)-> app_start(X, ServiceName, StartMode) end,
                 HandleServices 
-              ),
-              {reply, ok, NewHandleServices}
-          end
+          ),
+          {reply, ok, NewHandleServices}
   end
   
 ;
